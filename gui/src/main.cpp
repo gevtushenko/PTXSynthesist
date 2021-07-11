ef4a3832a2cd92e15af95cf586aad96e8e154403
Georgy Evtushenko <evtushenko.georgy@gmail.com>
2021-07-11 22:12:12 +0300

    Show maximized

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
   QApplication a(argc, argv);
 
   MainWindow main_window;
-  main_window.show();
+  main_window.showMaximized();
 
   return QApplication::exec();
 }
